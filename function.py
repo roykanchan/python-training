82539bda0dc5a0ca81b33fe5bb46df397204d997
Kanchan Roy <roykanchan08@gmail.com>
2026-03-02 00:44:31 +0530

    finction_return

---
+++ function.py
@@ -0,0 +1,23 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    s = num1 - num2
+    print(s)
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+def div():
+    d = num1 = num2
+    print(d)
+
+addition()
+sub()
+div()
+mul()
---
+++ function_return.py
@@ -0,0 +1,20 @@
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+
+def mul(num1, num2):
+    m = num1 * num2
+    return m
+
+def div(num1, num2):
+    d = num1/num2
+    return d
+
+print(addition(10,5))
+print(sub(10,5))
+print(mul(10,5))
+print(div(10,5))
