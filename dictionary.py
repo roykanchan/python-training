55df0931c3c73510aea51990163c577c6759a12a
Kanchan Roy <roykanchan08@gmail.com>
2026-03-03 21:21:39 +0530

    multiple_values

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,22 @@
-student_info = {
-    "name": "Abhi",
-    "Age": 15,
-    "Class": 12
-}
-print(student_info["Age"])
+# student_info = {
+#     "name": "Abhi",
+#     "Age": 15,
+#     "Class": 12
+# }
+# print(student_info["Age"])
+
+#For multiple values:
+
+students_info = [
+    {
+        "name": "Abhi",
+        "Age": 15,
+        "Class": 12
+    },
+    {
+        "name": "Rohit",
+        "Age": 16,
+        "Class": 11
+    },
+]
+print(students_info[1]["name"])
